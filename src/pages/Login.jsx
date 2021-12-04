289ed235efcc372c9d8b7ef60c3527682340d866
wind_create <nardowilli@gmail.com>
2021-12-04 18:33:39 +0700

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,57 +3,42 @@ import shoppedia from '../assets/images/image1.png'
 import { Box } from '@chakra-ui/layout'
 import { Icon } from "@chakra-ui/react"
 import { SiShopify } from "react-icons/si"
+import { rules } from '../components/ValidationLogin';
 
 
 const Login = () => {
     return (
-        <div className="2xl:container h-screen m-auto">
-            <div className="fixed insert-0 w-6/12 h-full lg:blockitems-center">
-                <img className="mt-20 px-20 " src={shoppedia} />
+        < div className="min-h-screen flex items-center justify-center bg-blue-400" >
+
+            <div className="bg-white p-16 rounded shadow-2xl w-2/3">
+
+                <a href="">
+                    <Box className="justify-center text-2xl mb-10 flex flex-row space-x-3 items-center">
+                        <Icon as={SiShopify} w="35px" h="35px" color="blue.400" />
+                        <p>Shop-Pedia</p>
+                    </Box>
+                    <p className="text-center text-lg text-gray-600">Welcome to Shop-Pedia !!</p>
+                </a>
+
+                <form className="space-y-5">
+
+                    <div>
+                        <label className="block mb-1 font-bold text-gray-500">Email</label>
+                        <input type="email" className="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-purple-500" />
+                    </div>
+
+                    <div>
+                        <label className="block mb-1 font-bold text-gray-500">Password</label>
+                        <input type="password" className="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-purple-500" />
+                    </div>
+
+
+                    <button className="block w-full bg-yellow-400 hover:bg-yellow-300 p-4 rounded text-yellow-900 hover:text-yellow-800 transition duration-300">Sign In</button>
+
+                </form>
+
             </div>
-            <div role="hidden" className="fixed inset-0 w-6/12 h-full ml-auto bg-white bg-opacity-70 backdrop-blur-xl lg:block"> </div>
 
-            <div className="relative ml-auto lg:w-7/12">
-                <div className="m-auto py-12 px-6 sm:p-20 xl:w-10/12">
-                    <div className="pt-6 space-y-2">
-                        <a href="">
-                            <Box className="justify-center text-2xl mb-10 flex flex-row space-x-3 items-center">
-                                <Icon as={SiShopify} w="35px" h="35px" color="blue.400" />
-                                <p>Shop-Pedia</p>
-                            </Box>
-                            <p className="text-center text-lg text-gray-600">Welcome to Shop-Pedia !!</p>
-                        </a>
-                    </div>
-                    <div className="mt-12 border-t">
-                        <span className="block w-max mx-auto -mt-3 px-4 text-gray-500 bg-white">Please Login</span>
-                    </div>
-
-                    <form action="" className="space-y-6">
-                        <div>
-                            <input type="email" placeholder="Your email" className="w-full px-6 py-3 rounded-xl ring-1 ring-gray-300 placeholder-gray-600 invalid:ring-red-500 focus:invalid:ring-red-500 focus:invalid:outline-none disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400"></input>
-                        </div>
-
-                        <div className="flex flex-col items-end">
-                            <input type="password" placeholder="Password" className="w-full px-6 py-3 rounded-xl ring-1 ring-gray-300 placeholder-gray-600 invalid:ring-red-500 focus:invalid:ring-red-500 focus:invalid:outline-none disabled:ring-gray-200 disabled:bg-gray-100 disabled:placeholder-gray-400"></input>
-
-                            <button className="p-3 -mr-3">
-                                <span className="text-blue-600">Forget Password</span>
-                            </button>
-                        </div>
-                        <div>
-                            <button type="submit" className="block w-full px-6 py-3 rounded-xl bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 active:bg-blue-800">
-                                <span className="text-lg text-white">Login</span>
-                            </button>
-                            <a href="" className="block w-max p-3 -ml-3">
-                                <span className="text-blue-600">Create New Account</span>
-                            </a>
-                        </div>
-                    </form>
-                    <div className="mt-12 pt-6 space-y-2 border-t">
-                        <span className="block text-center text-gray-500">Buy Something Now!!</span>
-                    </div>
-                </div>
-            </div >
         </div >
     )
 }
