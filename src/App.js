234adb8b446943ffe7674bc25753c9bd75036081
Fajar Julianto <slashcrackdie@gmail.com>
2021-12-05 19:18:09 +0700

    fix route

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
+import React,{ Suspense } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import LayoutMain from './layouts/LayoutMain';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import "slick-carousel/slick/slick.css";
-import React from 'react';
 import "slick-carousel/slick/slick-theme.css";
-import Login from './pages/Login';
 import { Provider } from 'react-redux';
 import store from './app/store';
 import { listen } from './app/listener';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import routes from './router/routes';
 
 function App() {
   React.useEffect(() => {
     listen();
   }, [])
+  const loading = (
+    <div className="pt-3 text-center">
+      <div className="sk-spinner sk-spinner-pulse"></div>
+    </div>
+  )
   return (
-    <Provider store={store}>
       <ChakraProvider>
         <Router>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<LayoutMain />} />
-          </Routes>
-        </Router>
+        <Suspense fallback={loading}>
+          <LayoutMain>
+            <Switch>
+                {
+                  routes.map((route,idx)=>{
+                    return (
+                        route.component && (
+                          <Route 
+                            exact={route.exact} key={idx} path={route.path} name={route.name} render={props => <route.component {...props} />}/>
+                        )
+                      )
+                  })
+                }
+            </Switch>
+          </LayoutMain>
+        </Suspense>
+      </Router>
       </ChakraProvider>
-    </Provider>
   );
 }
 
