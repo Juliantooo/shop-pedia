5c2ee5bf59188a7b912f4f4c3026a13080c9d354
Fajar Julianto <slashcrackdie@gmail.com>
2021-12-05 19:19:53 +0700

    implement api

--- src/components/PopularByCategory.jsx
+++ src/components/PopularByCategory.jsx
@@ -5,6 +5,7 @@ import '../assets/css/modules/reactSlick.css'
 import { AiOutlinePlusCircle } from "react-icons/ai"
 import Icon from '@chakra-ui/icon';
 import { http } from '../libs/services/http';
+import { useHistory } from "react-router-dom";
 
 const settings = {
     infinite: true,
@@ -32,15 +33,19 @@ const settings = {
     ]
 };
 
-
-const PopularByCategory = () => {
+const PopularByCategory = ({category}) => {
+    const history = useHistory()
     const [products, setProducts] = useState([]);
 
     const fetchData = async () =>{
-        const response = await http('get')
-        const data = response.data.filter((item)=> item.id <= 6)
+        const response = await http('get',`category/${category}`)
+        const data = response.data.filter((item,idx)=> {if(idx <= 4) return item})
         setProducts(data)
-        console.log(products)
+    }
+
+
+    const handleClick=(id)=>{
+        history.push(`/product/${id}`);
     }
 
     useEffect(() => {
@@ -50,15 +55,23 @@ const PopularByCategory = () => {
     return (
         <div className="space-y-5">
             <div className="flex flex-row justify-between items-center">
-                <h1 className="text-xl font-semibold text-gray-600">Electronics Terpopuler</h1>
-                <Icon as={AiOutlinePlusCircle} w="24px" h="24px" color="blue.400" className="cursor-pointer"/>
+                <h1 className="text-xl font-semibold text-gray-600">{category} Terpopuler</h1>
+                <Icon as={AiOutlinePlusCircle} w="24px" h="24px" color="blue.400" className="cursor-pointer" />
             </div>
             <Slider {...settings}>
                 {
                     products.length>0&&products.map((product,idx)=>{
                         return (
                             <div key={idx}>
-                                <CardProduct category={product.category} image={product.image} title={product.title} price={product.price} rating={product.rating}/>
+                                <CardProduct 
+                                category={product.category} 
+                                image={product.image} 
+                                title={product.title} 
+                                price={product.price} 
+                                rating={product.rating} 
+                                id={product.id}
+                                handleClick={handleClick}
+                                />
                             </div>
                         )
                     })
