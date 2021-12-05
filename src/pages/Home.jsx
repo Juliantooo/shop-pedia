c725f2ea599580d34635eda710a21cbb3a21a087
Fajar Julianto <slashcrackdie@gmail.com>
2021-12-05 19:22:29 +0700

    modify home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Slider from "react-slick";
 import '../assets/css/modules/reactSlick.css'
 import PopularByCategory from '../components/PopularByCategory';
@@ -7,6 +7,7 @@ import Banner2 from '../assets/images/Mask Group.png';
 import Banner3 from '../assets/images/W5.png';
 import Banner4 from '../assets/images/W6.png';
 import { Image } from '@chakra-ui/image';
+import { http } from '../libs/services/http';
 
 const settings = {
     dots: true,
@@ -21,6 +22,17 @@ const settings = {
 const banners = [Banner1,Banner2,Banner3,Banner4]
 
 const Home = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    const fetchData = async () =>{
+        const response = await http('get','categories')
+        setCategories(response.data)
+    }
+    useEffect(() => {
+        fetchData();
+    }, [])
+
     return (
         <div className="w-full p-2 md:p-12 space-y-8 md:space-y-16">
             <Slider {...settings}>
@@ -31,9 +43,9 @@ const Home = () => {
                 }
             </Slider>
             <div className="space-y-5">
-                <PopularByCategory/>
-                <PopularByCategory/>
-                <PopularByCategory/>
+                {
+                    categories.map((category)=> <PopularByCategory key={category} category={category}/> )
+                }
             </div>
         </div>
     )
