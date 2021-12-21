1aab0f70bac4c9c738b965c966d37c23ccac4d92
Fajar Julianto <slashcrackdie@gmail.com>
2021-12-21 17:15:42 +0700

    add slicer orders

---
+++ src/store/slicers/orders.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    orderItems: []
+}
+
+export const orderItems = createSlice({
+    name: 'orderItems',
+    initialState,
+    reducers: {
+        ADD_ORDER_ITEMS: (state, action) => {
+            const localOrders = JSON.parse(localStorage.getItem('orderItems'))
+            state.orderItems = localOrders;
+            state.orderItems.push(action.payload);
+            localStorage.setItem('orderItems', JSON.stringify(state.orderItems))
+        }
+    }
+})
+
+export const {
+    ADD_ORDER_ITEMS,
+} = orderItems.actions
+
+export default orderItems.reducer
