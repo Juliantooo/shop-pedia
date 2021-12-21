037367ead46de3b2c8f3d33b5fc77debb0c636ff
Fajar Julianto <slashcrackdie@gmail.com>
2021-12-21 17:10:48 +0700

    modify other popular by category

--- src/components/PopularByCategory.jsx
+++ src/components/PopularByCategory.jsx
@@ -6,16 +6,18 @@ import { AiOutlinePlusCircle } from "react-icons/ai"
 import Icon from '@chakra-ui/icon';
 import { http } from '../libs/services/http';
 import { useHistory } from "react-router-dom";
+import useProducts from '../hooks/products';
+import { useSelector } from 'react-redux';
 
 const settings = {
     infinite: true,
     speed: 500,
     slidesToShow: 2,
     slidesToScroll: 1,
-    autoplay:true,
-    arrows:false,
+    autoplay: true,
+    arrows: false,
     variableWidth: true,
-    responsive:[
+    responsive: [
         {
             breakpoint: 1024,
             settings: {
@@ -33,24 +35,24 @@ const settings = {
     ]
 };
 
-const PopularByCategory = ({category}) => {
-    const history = useHistory()
-    const [products, setProducts] = useState([]);
+const PopularByCategory = ({ category }) => {
+    const [products, setproducts] = useState([])
+    const allProducts = useSelector(state => state.products.products);
 
-    const fetchData = async () =>{
-        const response = await http('get',`category/${category}`)
-        const data = response.data.filter((item,idx)=> {if(idx <= 4) return item})
-        setProducts(data)
+    const history = useHistory()
+
+    const handleClick = (id) => {
+        history.push(`/${id}`);
     }
 
-
-    const handleClick=(id)=>{
-        history.push(`/product/${id}`);
+    const filterProductsByCategory = () => {
+        const filteredProducts = allProducts.filter((product) => product.category === category);
+        setproducts(filteredProducts)
     }
 
     useEffect(() => {
-        fetchData();
-    }, [])
+        filterProductsByCategory()
+    }, [allProducts])
 
     return (
         <div className="space-y-5">
@@ -60,17 +62,18 @@ const PopularByCategory = ({category}) => {
             </div>
             <Slider {...settings}>
                 {
-                    products.length>0&&products.map((product,idx)=>{
+                    products.length > 0 && products.map((product, idx) => {
                         return (
                             <div key={idx}>
-                                <CardProduct 
-                                category={product.category} 
-                                image={product.image} 
-                                title={product.title} 
-                                price={product.price} 
-                                rating={product.rating} 
-                                id={product.id}
-                                handleClick={handleClick}
+                                <CardProduct
+                                    category={product.category}
+                                    image={product.image}
+                                    title={product.title}
+                                    price={product.price}
+                                    rating={product.rating}
+                                    id={product.id}
+                                    stock={product.stock}
+                                    handleClick={handleClick}
                                 />
                             </div>
                         )
