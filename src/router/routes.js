f2fde37b47b9fe33f65a677d43caf775fadcbc33
Fajar Julianto <slashcrackdie@gmail.com>
2021-12-21 17:15:09 +0700

    add new routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,49 +1,75 @@
-import React  from "react"  
+import React from "react"
 import routesName from "./routesName"
 
 
-const routes=[
+const routes = [
     {
-        path:'/login',
-        name:routesName.LOGIN,
-        component: React.lazy(()=> import('../pages/Login'))
+        path: '/login',
+        name: routesName.LOGIN,
+        protectedRoute: false,
+        component: React.lazy(() => import('../pages/Login'))
     },
     {
-        path:'/mens-cloth',
-        name:routesName.MENS_CLOTH,
-        component: React.lazy(()=> import('../pages/MensCloth'))
+        path: '/mens-cloth',
+        name: routesName.MENS_CLOTH,
+        useLayout: true,
+        component: React.lazy(() => import('../pages/MensCloth'))
     },
     {
-        path:'/jewelery',
-        name:routesName.JEWELERY,
-        component: React.lazy(()=> import('../pages/Jewelery'))
+        path: '/jewelery',
+        name: routesName.JEWELERY,
+        useLayout: true,
+        component: React.lazy(() => import('../pages/Jewelery'))
     },
     {
-        path:'/electronics',
-        name:routesName.ELECTRONICS,
-        component: React.lazy(()=> import('../pages/Eectronics'))
+        path: '/electronics',
+        name: routesName.ELECTRONICS,
+        useLayout: true,
+        component: React.lazy(() => import('../pages/Eectronics'))
     },
     {
-        path:'/womens-cloth',
-        name:routesName.WOMENS_CLOTH,
-        component: React.lazy(()=> import('../pages/WomensCloth'))
+        path: '/womens-cloth',
+        name: routesName.WOMENS_CLOTH,
+        useLayout: true,
+        component: React.lazy(() => import('../pages/WomensCloth'))
     },
     {
-        path:'/searched',
-        name:routesName.SEARCHED,
-        component: React.lazy(()=> import('../pages/Searched'))
+        path: '/searched',
+        name: routesName.SEARCHED,
+        useLayout: true,
+        component: React.lazy(() => import('../pages/Searched'))
     },
     {
-        path:'/product/:id',
-        name:routesName.DETAIL_PRODUCTS,
-        component: React.lazy(()=> import('../pages/DetailProduct'))
+        path: '/stock',
+        useLayout: true,
+        name: routesName.UPDATE_STOCK,
+        component: React.lazy(() => import('../pages/EditStock'))
     },
     {
-        path:'/',
-        name:routesName.HOME,
-        component: React.lazy(()=> import('../pages/Home')),
-        exact : true
-    }
+        path: '/report',
+        useLayout: true,
+        name: routesName.REPORT,
+        component: React.lazy(() => import('../pages/Report'))
+    },
+    {
+        path: '/cart',
+        name: routesName.CART,
+        useLayout: true,
+        component: React.lazy(() => import('../pages/Cart'))
+    },
+    {
+        path: '/:id',
+        name: routesName.DETAIL_PRODUCTS,
+        useLayout: true,
+        component: React.lazy(() => import('../pages/DetailProduct'))
+    },
+    {
+        path: '/',
+        name: routesName.HOME,
+        useLayout: true,
+        component: React.lazy(() => import('../pages/Home')),
+        exact: true
+    },
 ]
 
 export default routes
--- src/router/routesName.js
+++ src/router/routesName.js
@@ -1,12 +1,15 @@
 const routesName = {
-    LOGIN:'login',
-    MENS_CLOTH:'mens-cloth',
-    JEWELERY:'jewelery',
-    ELECTRONICS:'electronics',
-    WOMENS_CLOTH:'womens-cloth',
-    DETAIL_PRODUCTS:'detail-product',
-    HOME:'home',
-    SEARCHED:'searched'
+    LOGIN: 'login',
+    MENS_CLOTH: 'mens-cloth',
+    JEWELERY: 'jewelery',
+    ELECTRONICS: 'electronics',
+    WOMENS_CLOTH: 'womens-cloth',
+    DETAIL_PRODUCTS: 'detail-product',
+    HOME: 'home',
+    SEARCHED: 'searched',
+    CART: 'cart',
+    REPORT: 'report',
+    UPDATE_STOCK: 'stock'
 }
 
 export default routesName
