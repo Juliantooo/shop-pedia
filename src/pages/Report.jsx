f7082afc3a210e802810aed822f0c90c4d1b785e
Fajar Julianto <slashcrackdie@gmail.com>
2021-12-21 17:34:51 +0700

    modify report

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -28,53 +28,60 @@ const Report = () => {
             })
             return [...result, ...item]
         }, [])
-        const earning = products.reduce((result, product) => result + product.price * product.count, 0);
+        const earning = products && products.reduce((result, product) => result + product.price * product.count, 0);
         setOrders(products);
         setTotalEarning(earning)
     }, [])
 
     return (
         <div className="w-full p-2 md:p-12 space-y-8 md:space-y-16">
-            <div className='flex flex-col items-center justify-between'>
-                <div className='grid grid-cols-4 gap-2 items-center w-full border p-5 border-gray-500'>
-                    <div className='text-center'>
-                        <p>Image</p>
-                    </div>
-                    <div className='text-left'>
-                        <p>Product Name</p>
-                    </div>
-                    <div className='text-right'>
-                        <p>Price</p>
-                    </div>
-                    <div className='text-right'>
-                        <p>Sold</p>
+            {orders && orders.length > 0
+                ?
+                <div className='flex flex-col items-center justify-between'>
+                    <div>
+                        <div className='grid grid-cols-4 gap-2 items-center w-full border p-5 border-gray-500'>
+                            <div className='text-center'>
+                                <p>Image</p>
+                            </div>
+                            <div className='text-left'>
+                                <p>Product Name</p>
+                            </div>
+                            <div className='text-right'>
+                                <p>Price</p>
+                            </div>
+                            <div className='text-right'>
+                                <p>Sold</p>
+                            </div>
+                        </div>
+                        {
+                            orders && orders.map((product, idx) => {
+                                return (
+                                    <div key={product.id} className={`${idx % 2 === 0 && 'bg-gray-200'} grid grid-cols-4 gap-2 items-center w-full border p-5 border-gray-500`} >
+                                        <div>
+                                            <img src={product.image} alt={product.title} className='h-20 object-contain mx-auto' />
+                                        </div>
+                                        <div className='text-left'>
+                                            <p>{product.title}</p>
+                                        </div>
+                                        <div className='text-right'>
+                                            <p>{product.price}</p>
+                                        </div>
+                                        <div className='text-right'>
+                                            <p>{product.count} items</p>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                        <div className='flex flex-row items-center justify-between p-5 border border-gray-500' style={{ margin: 0 }}>
+                            <p className='font-bold'>Total earning</p>
+                            <p>${totalEarning}</p>
+                        </div>
                     </div>
                 </div>
-                {
-                    orders && orders.length > 0 && orders.map((product, idx) => {
-                        return (
-                            <div key={product.id} className={`${idx % 2 === 0 && 'bg-gray-200'} grid grid-cols-4 gap-2 items-center w-full border p-5 border-gray-500`} >
-                                <div>
-                                    <img src={product.image} alt={product.title} className='h-20 object-contain mx-auto' />
-                                </div>
-                                <div className='text-left'>
-                                    <p>{product.title}</p>
-                                </div>
-                                <div className='text-right'>
-                                    <p>{product.price}</p>
-                                </div>
-                                <div className='text-right'>
-                                    <p>{product.count} items</p>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-            <div className='flex flex-row items-center justify-between p-5 border border-gray-500' style={{ margin: 0 }}>
-                <p className='font-bold'>Total earning</p>
-                <p>${totalEarning}</p>
-            </div>
+                :
+                <p className='text-2xl font-semibold text-left'>Belum ada Penjualan</p>
+            }
         </div >
     )
 }
