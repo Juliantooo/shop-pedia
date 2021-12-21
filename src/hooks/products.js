328616597864706e8d842ee3632d33a3bf512942
Fajar Julianto <slashcrackdie@gmail.com>
2021-12-21 17:11:14 +0700

    modify product hooks

--- src/hooks/products.js
+++ src/hooks/products.js
@@ -1,4 +1,5 @@
 import  { useEffect, useState } from 'react'
+import { randomNumber } from '../libs/helpers/randomNumber';
 import { http } from '../libs/services/http';
 
 const useProducts = (method,url)=>{
@@ -6,12 +7,17 @@ const useProducts = (method,url)=>{
 
     const fetchData = async () =>{
         const response = await http(method,url);
-        setProducts(response.data)
+        const modifiedProducts = response.data.length > 0
+        ? response.data.map((item)=>{
+            return {...item,stock:randomNumber(7,14)}
+        })
+        : {...response.data,stock:randomNumber(7,14)}
+        setProducts(modifiedProducts)
     }
 
     useEffect(() => {
         fetchData();
-    }, [])
+    }, [url])
 
     return{
     products
