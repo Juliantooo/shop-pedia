18ca417d3289bc1bd53eea724a2f8b6b6dd0b2f3
Fajar Julianto <slashcrackdie@gmail.com>
2021-12-21 18:35:31 +0700

    small change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Suspense } from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import routes from './router/routes';
 import UseLayout from './hooks/useLayout';
+import { ChakraProvider } from '@chakra-ui/react';
 import './assets/css/tailwind.css';
 
 function App() {
