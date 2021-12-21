50a500cdc9a31388ca5eb7b4b456fd17f85da050
Fajar Julianto <slashcrackdie@gmail.com>
2021-12-21 17:13:26 +0700

    modify home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,9 @@ import Banner3 from '../assets/images/W5.png';
 import Banner4 from '../assets/images/W6.png';
 import { Image } from '@chakra-ui/image';
 import { http } from '../libs/services/http';
+import { useDispatch } from 'react-redux';
+import { ADD_ALL_PRODUCTS } from '../store/slicers/products';
+import useProducts from '../hooks/products';
 
 const settings = {
     dots: true,
@@ -15,20 +18,34 @@ const settings = {
     speed: 500,
     slidesToShow: 2,
     slidesToScroll: 1,
-    autoplay:true,
+    autoplay: true,
     arrows: null,
 };
 
-const banners = [Banner1,Banner2,Banner3,Banner4]
+const banners = [Banner1, Banner2, Banner3, Banner4]
+const URL = 'products'
+const GET = 'get'
 
 const Home = () => {
+    const { products } = useProducts(GET, URL);
+    const dispatch = useDispatch()
 
     const [categories, setCategories] = useState([]);
 
-    const fetchData = async () =>{
-        const response = await http('get','categories')
+
+    const fetchData = async () => {
+        const response = await http('get', 'products/categories')
         setCategories(response.data)
     }
+
+    useEffect(() => {
+        const localProducts = JSON.parse(localStorage.getItem('allProducts'));
+        if (localProducts && localProducts.length > 0) {
+            return dispatch(ADD_ALL_PRODUCTS(localProducts))
+        }
+        dispatch(ADD_ALL_PRODUCTS(products))
+    }, [products])
+
     useEffect(() => {
         fetchData();
     }, [])
@@ -37,14 +54,14 @@ const Home = () => {
         <div className="w-full p-2 md:p-12 space-y-8 md:space-y-16">
             <Slider {...settings}>
                 {
-                    banners.map((banner,idx)=>{
-                        return <Image src={banner} alt={`Banner ${idx+1}`} key={idx} className="object-fit" />
+                    banners.map((banner, idx) => {
+                        return <Image src={banner} alt={`Banner ${idx + 1}`} key={idx} className="object-fit" />
                     })
                 }
             </Slider>
             <div className="space-y-5">
                 {
-                    categories.map((category)=> <PopularByCategory key={category} category={category}/> )
+                    categories.map((category) => <PopularByCategory key={category} category={category} />)
                 }
             </div>
         </div>
