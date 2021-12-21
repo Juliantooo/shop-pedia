784dbeb360cc0b011732e5a4f166b9d41e95d2b5
Fajar Julianto <slashcrackdie@gmail.com>
2021-12-21 17:15:25 +0700

    add slicer

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
+import cartItems from './slicers/cart'
+import  orderItems from './slicers/orders'
+import  products from './slicers/products'
 
 export const store = configureStore({
     reducer: {
-
+        products,
+        cartItems,
+        orderItems,
     },
 })
