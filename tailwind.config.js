7bdf79218a91adebb7ce1244e3bb63786e9c18a5
Fajar Julianto <slashcrackdie@gmail.com>
2021-12-27 16:39:52 +0700

    add screen breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 // tailwind.config.js
 module.exports = {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
