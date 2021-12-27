dc1f9571ccbc0007769f2c3a64ed7e886bbd093c
Fajar Julianto <slashcrackdie@gmail.com>
2021-12-27 17:30:42 +0700

    small changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,6 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
     extend: {},
   },
   variants: {
