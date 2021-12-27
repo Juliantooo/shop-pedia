5e1e40abeb2d647b018500040c5ceade336271ef
Fajar Julianto <slashcrackdie@gmail.com>
2021-12-27 16:19:10 +0700

    fix tag

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -60,25 +60,25 @@ const Sidebar = ({ isOpen }) => {
 
     return (
         <div className={`w-72 h-full p-6 text-gray-600 ${isOpen ? 'block bg-white md:bg-gray-100 z-50 md:z-0 h-screen md:h-auto fixed md:relative top-0 md:top-auto' : 'hidden'} md:block`}>
-            <Stack direction={"column"} spacing="35px">
-                <Box className="text-2xl mb-10 flex flex-row space-x-3 items-center cursor-pointer" onClick={() => history.push('/')}>
+            <div className="flex flex-col space-y-8">
+                <div className="text-2xl mb-10 flex flex-row space-x-3 items-center cursor-pointer" onClick={() => history.push('/')}>
                     <Icon as={SiShopify} w="35px" h="35px" color="blue.400" />
                     <p>Shop-Pedia</p>
-                </Box>
-                <Box className="font-semibold text-xl" mb="2">
+                </div>
+                <div className="font-semibold text-xl" mb="2">
                     Categories
-                </Box>
+                </div>
                 {
                     menus.map((menu, idx) => {
                         return (
-                            <Box onClick={() => history.push({ pathname: menu.route })} key={idx} className="flex flex-row space-x-3 items-center cursor-pointer">
+                            <div onClick={() => history.push({ pathname: menu.route })} key={idx} className="flex flex-row space-x-3 items-center cursor-pointer">
                                 <Icon as={menu.icon} w="20px" h="20px" color="blue.400" />
                                 <p className="text-base">{menu.name}</p>
-                            </Box>
+                            </div>
                         )
                     })
                 }
-            </Stack>
+            </div>
         </div>
     )
 }
